749d27cbd7c4dd2332c27aa5787e767d1efffd49
sqljim <jim.m257@gmail.com>
2020-06-27 17:30:39 +0530

    Getting all tests to run successfully

--- src/app/modules/emergency-register/services/board-socket.service.spec.ts
+++ src/app/modules/emergency-register/services/board-socket.service.spec.ts
@@ -2,13 +2,19 @@ import { TestBed } from '@angular/core/testing';
 
 import { MessagingService } from './board-socket.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+
 
 describe('BoardSocketService', () => {
   let service: MessagingService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule,
+        AngularFireMessagingModule,
+        AngularFireModule.initializeApp(environment.firebase), ],
       providers: [MessagingService]
     });
     service = TestBed.inject(MessagingService);
