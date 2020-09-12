2b24ff72904df93cf5a091f4868919161e99fffb
arpit trivedi <35517286+arpit9612@users.noreply.github.com>
2020-09-12 17:03:23 +0530

    adding further functionality to the reporting tab now user can click on the areas and see the animal details for all the animals in that area

---
+++ src/app/modules/reporting/reporting-page-routing.module.ts
@@ -0,0 +1,18 @@
+import { ReportingPageComponent } from './pages/reporting-page/reporting-page.component';
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: ReportingPageComponent,
+        data: { shouldReuse: true, key: 'reporting' },
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class ReportingPageRoutingModule {}
--- src/app/pages/reporting-page/reporting-page.module.ts
+++ src/app/pages/reporting-page/reporting-page.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material-module';
 import { ReportingPageRoutingModule } from './reporting-page-routing.module';
 import { ReportingPageComponent } from './reporting-page.component';
+import { ReportingRecordModule } from "src/app/modules/reporting/components/reporting-record/reporting-record.module";
+
 
 @NgModule({
     declarations: [ReportingPageComponent],
-    imports: [CommonModule, ReportingPageRoutingModule,MaterialModule],
+
+    imports: [CommonModule, 
+        ReportingPageRoutingModule,
+        ReportingRecordModule,
+        MaterialModule
+    ],
+    exports:[ReportingPageComponent]
 })
 export class ReportingPageModule {}
