383d47c471b4ee454faa8b0163b8f96b22c6386e
arpit trivedi <35517286+arpit9612@users.noreply.github.com>
2020-10-30 17:46:35 +0530

    UserAdmin page works now can add a user abd also update record . The table support paging and filter also added some css for the looks of table

--- src/app/core/services/user-details/user-action.service.ts
+++ src/app/core/services/user-details/user-action.service.ts
@@ -1,25 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { APIService } from '../http/api.service';
+import { Observable } from 'rxjs';
+import { UserDetails } from 'src/app/pages/users-page/users-page.component';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserActionService extends APIService{
-endpoint = 'UserAdmin';
-constructor(public http: HttpClient) { 
-  super(http);
-}
-
-
-public async insertUser(userDetails:any): Promise<any> {
-  if (userDetails.userUserId) {
-    console.log('i was called');
-      return await this.put(userDetails);
-  } else {
-    console.log('no was called');
-      return await this.post(userDetails);
+  endpoint = 'UserAdmin';
+  constructor(public http: HttpClient) { 
+    super(http);
   }
-}
+
+
+  public async insertUser(userDetails:any): Promise<any> {
+    if (userDetails.userUserId) {
+      return await this.put(userDetails);
+    } else {
+      return await this.post(userDetails);
+    }
+  }
+
+  public getUsersByIdRange(): Observable<UserDetails[]> {
+    const request = '?GetUsersByIdRange';
+    return this.getObservable(request).pipe(
+      map((response:UserDetails[]) => {
+          return response;
+      }),
+  );
+
+  }
+
 
 }
