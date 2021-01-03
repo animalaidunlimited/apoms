b9e0acca4e7330ceb3ced99d04ff48a5a7bf0f0d
Jim Mackenzie <42493410+sqljim@users.noreply.github.com>
2021-01-03 09:54:24 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,22 +2,6 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//     production: false,
-//     API_URL: '',
-//     firebase: {
-//       apiKey: 'AIzaSyB1FEu4JYuoJhxeUpYmNDq8t5EeI3RjAl4',
-//       authDomain: 'streettreat.firebaseapp.com',
-//       databaseURL: 'https://streettreat.firebaseio.com',
-//       projectId: 'streettreat',
-//       storageBucket: 'streettreat.appspot.com',
-//       messagingSenderId: '275309609166',
-//       appId: '1:275309609166:web:57881f3451f618d031909f',
-//       measurementId: 'G-2FQTQ26YCP',
-//       email: 'arpit@animalaidunlimited.org',
-//       password: 'Arpit@AAU2020' }
-// };
-
 export const environment = {
   production: false,
   API_URL: '',
@@ -30,8 +14,8 @@ export const environment = {
     messagingSenderId: '275309609166',
     appId: '1:275309609166:web:57881f3451f618d031909f',
     measurementId: 'G-2FQTQ26YCP',
-    email: 'arpit@animalaidunlimited.org',
-    password: 'Arpit@AAU2020' }
+    email: '@animalaidunlimited.org',
+    password: '' }
 };
 
 /*
