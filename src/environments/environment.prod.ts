44753666738a2d1132f71cba5ec9b6070ab77266
arpit trivedi <35517286+arpit9612@users.noreply.github.com>
2021-01-06 09:49:15 +0530

    Changes to deploy the app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_URL: 'https://staging-dot-streettreat.el.r.appspot.com',
+    API_URL: 'https://streettreat.el.r.appspot.com',
     firebase: {
       apiKey: 'AIzaSyB1FEu4JYuoJhxeUpYmNDq8t5EeI3RjAl4',
       authDomain: 'streettreat.firebaseapp.com',
@@ -10,7 +10,7 @@ export const environment = {
       messagingSenderId: '275309609166',
       appId: '1:275309609166:web:57881f3451f618d031909f',
       measurementId: 'G-2FQTQ26YCP',
-      email: '@animalaidunlimited.org',
-      password: '' }
+      email: 'jim.mackenzie@animalaidunlimited.org',
+      password: 'L33ds@AAU'}
 
 };
