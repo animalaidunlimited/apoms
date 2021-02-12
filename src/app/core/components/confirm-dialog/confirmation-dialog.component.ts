6f77026571f35aa61ed1428de1c3864728498248
arpit trivedi <35517286+arpit9612@users.noreply.github.com>
2021-02-12 12:32:54 +0530

    Fixed issues related with the release and street treat

--- src/app/core/components/confirm-dialog/confirmation-dialog.component.ts
+++ src/app/core/components/confirm-dialog/confirmation-dialog.component.ts
@@ -1,6 +1,18 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
+
+export interface DialogData {
+  message: string;
+  buttonText: ButtonText;
+}
+
+interface ButtonText {
+  ok: string;
+  cancel: string;
+}
+
+
 @Component({
   selector: 'confirmation-dialog',
   templateUrl: 'confirmation-dialog.html',
@@ -12,15 +24,17 @@ export class ConfirmationDialog {
   confirmButtonText = 'Yes';
   cancelButtonText = 'Cancel';
 
+  get vals() {
+    return this.message.split('\n');
+  }
+
   constructor(
-    @Inject(MAT_DIALOG_DATA) private data: any,
+    @Inject(MAT_DIALOG_DATA) private data: DialogData,
     private dialogRef: MatDialogRef<ConfirmationDialog>) {
       if(data){
-    this.message = data.message || this.message;
-    if (data.buttonText) {
-      this.confirmButtonText = data.buttonText.ok || this.confirmButtonText;
-      this.cancelButtonText = data.buttonText.cancel || this.cancelButtonText;
-    }
+        this.message = data.message || this.message;
+        this.confirmButtonText = data.buttonText.ok || this.confirmButtonText;
+        this.cancelButtonText = data.buttonText.cancel || this.cancelButtonText;
       }
   }
 
