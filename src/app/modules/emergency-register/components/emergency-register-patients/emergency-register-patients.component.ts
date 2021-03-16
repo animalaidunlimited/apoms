c4c5ac6b77ac9ed843d2639970d2cfd9bdf6e25d
Ankit Singh <ankitsingh.udaipur@gmail.com>
2021-03-16 14:47:30 +0530

    Module Not working

---
+++ src/app/modules/emergency-register/components/emergency-register-patients/emergency-register-patients.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emergency-register-patients',
+  templateUrl: './emergency-register-patients.component.html',
+  styleUrls: ['./emergency-register-patients.component.scss']
+})
+export class EmergencyRegisterPatientsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
