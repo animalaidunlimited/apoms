ac3db0d4f3d92ca81e2f295b595bbe1d6a93673b
Ankit Singh <ankitsingh.udaipur@gmail.com>
2021-03-18 17:44:05 +0530

    Working Changes

--- src/app/modules/emergency-register/components/emergency-register-patient/emergency-register-patient.component.ts
+++ src/app/modules/emergency-register/components/emergency-register-patient/emergency-register-patient.component.ts
@@ -81,6 +81,8 @@ export class EmergencyRegisterPatientComponent implements OnInit {
     this.exclusions = this.dropdown.getExclusions();
 
 
+    console.log(this.patientForm);
+
     this.animalType = this.patientForm.get('animalType') as AbstractControl;
     this.filteredAnimalTypes$ = this.animalType.valueChanges.pipe(
       startWith(''),
