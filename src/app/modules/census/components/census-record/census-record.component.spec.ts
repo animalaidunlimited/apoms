756ba2b95320a08d6bb5ee0a18c88b3f256bcb5a
sqljim <jim.m257@gmail.com>
2021-03-21 11:54:43 +0530

    Renaming Census to census

---
+++ src/app/modules/census/components/census-record/census-record.component.spec.ts
@@ -0,0 +1,41 @@
+import { Overlay, OverlayModule } from '@angular/cdk/overlay';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CensusRecordComponent } from './census-record.component';
+
+describe('CensusRecordComponent', () => {
+  let component: CensusRecordComponent;
+  let fixture: ComponentFixture<CensusRecordComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        OverlayModule,
+        FormsModule
+      ],
+      providers: [MatSnackBar, Overlay],
+      declarations: [ CensusRecordComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(inject([FormBuilder], (fb: FormBuilder) => {
+
+    fixture = TestBed.createComponent(CensusRecordComponent);
+    component = fixture.componentInstance;
+
+    component.censusUpdateDate = new Date();
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/census/pages/census-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'census-page',
+    templateUrl: './census-page.component.html',
+    styleUrls: ['./census-page.component.scss'],
+})
+export class CensusPageComponent implements OnInit {
+ constructor(
+    ) {}
+
+    ngOnInit() {}
+
+}
