2b1caa480009d901fa87789626206da218f43958
sqljim <jim.m257@gmail.com>
2021-03-22 17:33:43 +0530

    Adding CD step to app engine

--- set-environment.ts
+++ set-environment.ts
@@ -10,7 +10,7 @@ https://medium.com/@ferie/how-to-pass-environment-variables-at-building-time-in-
 
 const { writeFile, existsSync, mkdirSync } = require('fs');
 // Configure Angular `environment.ts` file path
-const targetPath = './src/environments/environment.ts';
+const environmentTargetPath = './src/environments/environment.ts';
 // Load node modules
 const colors = require('colors');
 
@@ -33,16 +33,52 @@ const envConfigFile = `export const environment = {
   };`;
 
 
-console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
-console.log(colors.grey(envConfigFile));
-writeFile(targetPath, envConfigFile, function (err:string) {
+console.log(colors.magenta('The file `environment.ts` will be updated.'));
+writeFile(environmentTargetPath, envConfigFile, function (err:string) {
    if (err) {
        throw console.error(err);
    } else {
-       console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+       console.log(colors.magenta(`Angular environment.ts file generated correctly at ${environmentTargetPath} \n`));
    }
 });
 
 console.log(colors.magenta('Updating the package.json file to build for the correct environment'));
 
+console.log(colors.magenta('Adding the current deployment environment, i.e. the service'));
+const appYamlPath = 'dist/app.yaml';
+
+const appYamlFile =`service: ${process.env.DEPLOYMENT_ENVIRONMENT}
+runtime: nodejs12
+env: standard
+
+resources:
+  cpu: 1
+  memory_gb: 0.5
+  disk_size_gb: 10
+
+handlers:
+- url: /
+  secure: always
+  static_files: apoms/index.html
+  upload: apoms/index.html
+
+#  Routing rules for resources, css, js, images etc. Any file with format filename.ext
+- url: /(.*\\.(.+))$
+  secure: always
+  static_files: apoms/\\1
+  upload: apoms/(.*\\.(.+))$
+
+#  Routing rule for Angular Routing
+- url: /(.*)
+  secure: always
+  static_files: apoms/index.html
+  upload: apoms/index.html`;
+
+  writeFile(appYamlPath, appYamlFile, function (err:string) {
+    if (err) {
+        throw console.error(err);
+    } else {
+        console.log(colors.magenta(`Angular app.yaml file generated correctly at ${appYamlPath}`));
+    }
+ });
 
