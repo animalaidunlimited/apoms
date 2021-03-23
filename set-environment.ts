4c0fdadd50d286db302aa8c9fe9cff733ad45cfd
sqljim <jim.m257@gmail.com>
2021-03-23 13:02:27 +0530

    Splitting app.yaml build to after the build has completed

--- set-environment.ts
+++ set-environment.ts
@@ -6,8 +6,6 @@ https://betterprogramming.pub/how-to-secure-angular-environment-variables-for-us
 https://medium.com/@ferie/how-to-pass-environment-variables-at-building-time-in-an-angular-application-using-env-files-4ae1a80383c
 */
 
-
-
 const { writeFile, existsSync, mkdirSync } = require('fs');
 // Configure Angular `environment.ts` file path
 const environmentTargetPath = './src/environments/environment.ts';
@@ -44,41 +42,4 @@ writeFile(environmentTargetPath, envConfigFile, function (err:string) {
 
 console.log(colors.magenta('Updating the package.json file to build for the correct environment'));
 
-console.log(colors.magenta('Adding the current deployment environment, i.e. the service'));
-const appYamlPath = 'dist/app.yaml';
-
-const appYamlFile =`service: ${process.env.DEPLOYMENT_ENVIRONMENT}
-runtime: nodejs12
-env: standard
-
-resources:
-  cpu: 1
-  memory_gb: 0.5
-  disk_size_gb: 10
-
-handlers:
-- url: /
-  secure: always
-  static_files: apoms/index.html
-  upload: apoms/index.html
-
-#  Routing rules for resources, css, js, images etc. Any file with format filename.ext
-- url: /(.*\\.(.+))$
-  secure: always
-  static_files: apoms/\\1
-  upload: apoms/(.*\\.(.+))$
-
-#  Routing rule for Angular Routing
-- url: /(.*)
-  secure: always
-  static_files: apoms/index.html
-  upload: apoms/index.html`;
-
-  writeFile(appYamlPath, appYamlFile, function (err:string) {
-    if (err) {
-        throw console.error(err);
-    } else {
-        console.log(colors.magenta(`Angular app.yaml file generated correctly at ${appYamlPath}`));
-    }
- });
 
