1a15560215bc799a7927ac7eab93a338c168dba2
sqljim <jim.m257@gmail.com>
2021-03-26 17:02:17 +0530

    Retrying to use secrets

--- set-app-yaml.ts
+++ set-app-yaml.ts
@@ -7,7 +7,7 @@ require('dotenv').load();
 
 const environment = JSON.stringify(process.env.DEPLOYMENT_ENVIRONMENT || 'other');
 
-console.log(process.env.DEPLOYMENT_ENVIRONMENT?.length);
+console.log(process.env.DEPLOYMENT_ENVIRONMENT?.trim().length);
 
 console.log(colors.magenta(`Setting the current deployment environment, i.e. the service, to ${environment}`));
 const appYamlPath = 'dist/app.yaml';
