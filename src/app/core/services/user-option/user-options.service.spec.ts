6059ac30171cc2ea0e1bb5980c878128330fea6a
sqljim <jim.m257@gmail.com>
2021-04-02 15:03:03 +0530

    Adding tests

--- src/app/core/services/user-option/user-options.service.spec.ts
+++ src/app/core/services/user-option/user-options.service.spec.ts
@@ -16,4 +16,26 @@ describe('UserOptionsService', () => {
         const service: UserOptionsService = TestBed.get(UserOptionsService);
         expect(service).toBeTruthy();
     });
+
+    it('Home should be Udaipur when blank', () => {
+        const service: UserOptionsService = TestBed.get(UserOptionsService);
+
+        const coords = service.getCoordinates();
+        const coordObject = {
+            lat: 24.57127,
+            lng: 73.691544,
+        };
+
+        expect(coords).toEqual(coordObject);
+
+    });
+
+    it('Default duration should be 5 seconds', () => {
+        const service: UserOptionsService = TestBed.get(UserOptionsService);
+
+        const notificationSeconds = service.getNotifactionDuration();
+
+        expect(notificationSeconds).toEqual(3);
+
+    });
 });
