ce20fd8e0387a1a79f47729e5b7a42b4fd6f8300
Ankit Singh <ankitsingh.udaipur@gmail.com>
2021-04-28 18:34:02 +0530

    fix for insert and update visit

--- scripts/database/Release/Release_1.18/Release_1.18.sql
+++ scripts/database/Release/Release_1.18/Release_1.18.sql
@@ -505,25 +505,20 @@ BEGIN
 Created By: Ankit Singh
 Created On: 02/12/2020
 Purpose: Used to insert a new case.
+
+
+Created By: Ankit Singh
+Created On: 27/04/2021
+Purpose: ON DUPLICATE KEY UPDATE Added
 */
 
-DECLARE vCaseNoExists INT;
-DECLARE vSuccess INT;
 DECLARE vStreetTreatCaseId INT;
+DECLARE vSuccess INT;
 DECLARE vOrganisationId INT;
 
 SELECT u.OrganisationId INTO vOrganisationId FROM AAU.User u WHERE UserName = prm_Username LIMIT 1;
 
-SET vCaseNoExists = 0;
-
-SELECT COUNT(1), StreetTreatCaseId INTO vCaseNoExists, vStreetTreatCaseId 
-FROM AAU.StreetTreatCase 
-WHERE PatientId = prm_PatientId GROUP BY PatientId;
-
-IF vCaseNoExists = 0 THEN
-
-	INSERT INTO AAU.StreetTreatCase
-						(
+INSERT INTO AAU.StreetTreatCase(
                         PatientId,
 						PriorityId,
 						StatusId,
@@ -534,9 +529,7 @@ IF vCaseNoExists = 0 THEN
                         ClosedDate,
                         EarlyReleaseFlag,
                         OrganisationId
-						)
-				VALUES
-						(
+					) VALUES (
                         prm_PatientId,
 						prm_PriorityId,
 						prm_StatusId,
@@ -547,40 +540,25 @@ IF vCaseNoExists = 0 THEN
                         prm_ClosedDate,
                         prm_EarlyReleaseFlag,
                         vOrganisationId
-						);
+						) ON DUPLICATE KEY UPDATE
+                        PriorityId			= prm_PriorityId,
+						StatusId			= prm_StatusId,
+						TeamId				= prm_TeamId,
+						MainProblemId		= prm_MainProblemId,
+						AdminComments		= prm_AdminComments,
+						OperatorNotes		= prm_OperatorNotes,
+						ClosedDate			= prm_ClosedDate,
+						EarlyReleaseFlag	= prm_EarlyReleaseFlag;
+                        
 	SELECT 1 INTO vSuccess;
-    SELECT LAST_INSERT_ID() INTO vStreetTreatCaseId;
     
+	SELECT StreetTreatCaseId INTO vStreetTreatCaseId FROM AAU.StreetTreatCase WHERE PatientId = prm_PatientId;
+
     UPDATE AAU.Patient SET Description = IFNULL(prm_AnimalDescription,'') WHERE PatientId = prm_PatientId;
     
 	INSERT INTO AAU.Logging (UserName, RecordId, ChangeTable, LoggedAction, DateTime)
-	VALUES (NULL,vStreetTreatCaseId,'Case','Insert', NOW());
-    
-ELSEIF vCaseNoExists > 0 THEN
-
-    UPDATE  AAU.StreetTreatCase
-    SET 
-		PriorityId			= prm_PriorityId,
-		StatusId			= prm_StatusId,
-		TeamId				= prm_TeamId,
-		MainProblemId		= prm_MainProblemId,
-		AdminComments		= prm_AdminComments,
-		OperatorNotes		= prm_OperatorNotes,
-		ClosedDate			= prm_ClosedDate,
-		EarlyReleaseFlag	= prm_EarlyReleaseFlag
-	WHERE
-		PatientId = prm_PatientId;
-        
-	UPDATE AAU.Patient SET Description = IFNULL(prm_AnimalDescription,'') WHERE PatientId = prm_PatientId;
-        
-	SELECT 2 INTO vSuccess;
-
-ELSE
-	SELECT 3 INTO vSuccess;
-END IF;
-
-SELECT vStreetTreatCaseId AS streetTreatCaseId, vSuccess AS success;
-
+	VALUES (NULL,vStreetTreatCaseId,'Case','Upsert', NOW());
+	SELECT vStreetTreatCaseId AS streetTreatCaseId, vSuccess AS success;
 END$$
 
 
