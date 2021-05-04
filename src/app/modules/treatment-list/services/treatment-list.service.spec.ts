6f89b1a38f749e94360bcca586efa33667605f89
sqljim <jim.m257@gmail.com>
2021-05-04 15:38:02 +0530

    Adding MaterialModule to treatment page componenet

--- src/app/modules/treatment-list/services/treatment-list.service.spec.ts
+++ src/app/modules/treatment-list/services/treatment-list.service.spec.ts
@@ -1,6 +1,6 @@
 import { Overlay } from '@angular/cdk/overlay';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -32,11 +32,19 @@ describe('TreatmentListService', () => {
     ]
     });
 
+  });
+
+  beforeEach(inject([FormBuilder], (fb: FormBuilder) => {
     service = TestBed.inject(TreatmentListService);
 
-  });
+}));
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  afterAll(() => {
+    TestBed.resetTestingModule();
+  });
+
 });
