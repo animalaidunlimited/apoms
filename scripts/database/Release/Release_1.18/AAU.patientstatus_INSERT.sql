133efbceec0af181ed2d5da0be6b91e7ae30ea2d
Ankit Singh <ankitsingh.udaipur@gmail.com>
2021-05-05 18:11:20 +0530

    Procedure to be updated

--- scripts/database/Release/Release_1.18/AAU.patientstatus_INSERT.sql
+++ scripts/database/Release/Release_1.18/AAU.patientstatus_INSERT.sql
@@ -1,1 +1,14 @@
-INSERT INTO AAU.patientstatus (`PatientStatusId`, `OrganisationId`, `PatientStatus`, `IsDeleted`) VALUES ('7', '1', 'StreetTreat', '0');
+DROP PROCEDURE IF EXISTS `?`;
+DELIMITER //
+CREATE PROCEDURE `?`()
+BEGIN
+DECLARE statusExsits INT;
+  DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN END;
+  SELECT count(1) INTO statusExsits FROM AAU.PatientStatus WHERE PatientStatus='StreetTreat';
+IF statusExsits = 0 THEN
+	INSERT INTO AAU.PatientStatus (`OrganisationId`, `PatientStatus`, `IsDeleted`) VALUES ('1', 'StreetTreat', '0');
+END IF;
+END //
+DELIMITER ;
+CALL `?`();
+DROP PROCEDURE `?`;	
