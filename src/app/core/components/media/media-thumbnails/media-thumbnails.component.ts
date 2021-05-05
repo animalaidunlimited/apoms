25d470a4d3bddc0e3e496ec6f9fcf187cfa7f7f5
Ankit Singh <ankitsingh.udaipur@gmail.com>
2021-05-05 19:44:39 +0530

    Working Media Gallery

--- src/app/core/components/media/media-thumbnails/media-thumbnails.component.ts
+++ src/app/core/components/media/media-thumbnails/media-thumbnails.component.ts
@@ -4,6 +4,8 @@ import { BehaviorSubject, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Image, MediaItem } from 'src/app/core/models/media';
 import { MediaPreviewComponent } from '../media-preview/media-preview.component';
+
+import { PatientService } from 'src/app/core/services/patient/patient.service';
 @Component({
     // tslint:disable-next-line: component-selector
     selector: 'media-thumbnails',
@@ -13,8 +15,12 @@ import { MediaPreviewComponent } from '../media-preview/media-preview.component'
 export class MediaThumbnailsComponent implements OnInit{
     @Input() gallery!:Image[];
     @Input() mediaData!:BehaviorSubject<MediaItem[]>;
+    mediaDataImages:MediaItem[] = [];
     private ngUnsubscribe = new Subject();
-    constructor (public dialog: MatDialog) {}
+    constructor (
+        public dialog: MatDialog,        
+        private patientService:PatientService
+    ) {}
 
     ngOnInit(): void {
         
@@ -23,19 +29,23 @@ export class MediaThumbnailsComponent implements OnInit{
         const orientation = image?.height && image?.width ?  image?.height > image?.width ? 'Landscape' : 'Potrait' : '';
         // console.log(orientation);
 
-      
+        this.patientService.getPatientMediaItemsByPatientId(this.mediaData?.value[0].patientId).pipe(takeUntil(this.ngUnsubscribe)).subscribe((mediaItems:MediaItem[]) => 
+            this.mediaDataImages = mediaItems
+        );
         const dialogRef = this.dialog.open(MediaPreviewComponent, {
             minWidth: '80vw',
             panelClass: 'media-preview-dialog',
             data: {
                 image,
-                mediaData: this.mediaData?.value.filter(media => media.patientMediaItemId === image.patientMediaItemId)[0]
+                mediaData: this.mediaDataImages.filter(media => media.patientMediaItemId === image.patientMediaItemId)[0]
             }
         });
-        const subscribeDialog = dialogRef.componentInstance.onUpdateMediaItem.pipe(takeUntil(this.ngUnsubscribe)).subscribe((mediaItem:MediaItem) => {
-            console.log(mediaItem); 
+        const onUpdateMedia = dialogRef.componentInstance.onUpdateMediaItem.subscribe(() => {
+            this.patientService.getPatientMediaItemsByPatientId(this.mediaData?.value[0].patientId).pipe(takeUntil(this.ngUnsubscribe)).subscribe((mediaItems:MediaItem[]) => 
+                this.mediaDataImages = mediaItems
+            );
         });
-        dialogRef.afterClosed().subscribe(() => subscribeDialog.unsubscribe());
+        dialogRef.afterClosed().subscribe(()=> onUpdateMedia.unsubscribe());
         
     }
     ngOnDestroy() {
