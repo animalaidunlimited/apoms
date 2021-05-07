75bbc10133c6987303bfbd5826850b5041526bf5
sqljim <jim.m257@gmail.com>
2021-05-07 11:51:52 +0530

    Resolving issue with tag link in treatment list and allowing to clear existing moves.

---
+++ scripts/database/FixFunctions/ResolveBrokenCaseDetails.sql
@@ -0,0 +1,40 @@
+
+
+SELECT EmergencyNumber
+FROM AAU.EmergencyCase
+WHERE EmergencyCaseId NOT IN (SELECT EmergencyCaseId FROM AAU.Patient);
+
+START TRANSACTION;
+
+INSERT INTO AAU.Patient (OrganisationId, EmergencyCaseId, Position, AnimalTypeId, PatientStatusId, PatientStatusDate)
+SELECT OrganisationId, EmergencyCaseId, 1, 10, 1, CURDATE()
+FROM AAU.EmergencyCase
+WHERE EmergencyCaseId NOT IN (SELECT EmergencyCaseId FROM AAU.Patient);
+
+-- COMMIT
+-- ROLLBACK
+
+START TRANSACTION;
+
+INSERT INTO AAU.PatientProblem (PatientId, OrganisationId, ProblemId)
+SELECT PatientId, OrganisationId, 42
+FROM AAU.Patient
+WHERE PatientId NOT IN (SELECT PatientId FROM AAU.PatientProblem);
+
+-- COMMIT
+
+
+INSERT INTO AAU.EmergencyCaller (EmergencyCaseId, CallerId, PrimaryCaller)
+SELECT EmergencyCaseId, -1, 1
+FROM AAU.EmergencyCase
+WHERE EmergencyCaseId NOT IN (SELECT EmergencyCaseId FROM AAU.EmergencyCaller);
+
+
+-- Let's see how many duplicate callers we have
+SELECT EmergencyCaseId
+FROM AAU.EmergencyCaller
+GROUP BY EmergencyCaseId, CallerId
+HAVING COUNT(1) > 1
+
+
+
