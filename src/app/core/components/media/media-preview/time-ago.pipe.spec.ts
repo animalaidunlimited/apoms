6aafd92caaa6efb82638e8f621c65d7222630de6
Ankit Singh <ankitsingh.udaipur@gmail.com>
2021-05-09 11:03:35 +0530

    Time ago changes

--- src/app/core/components/media/media-preview/time-ago.pipe.spec.ts
+++ src/app/core/components/media/media-preview/time-ago.pipe.spec.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectorRef } from '@angular/core';
 import { TimeAgoPipe } from './time-ago.pipe';
 
 describe('TimeAgoPipe', () => {
-  let ref!:ChangeDetectorRef;
+
   it('create an instance', () => {
-    const pipe = new TimeAgoPipe(ref);
+    const pipe = new TimeAgoPipe();
     expect(pipe).toBeTruthy();
   });
 });
