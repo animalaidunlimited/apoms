42709c9298693b39e74764c3eaae9c233286cb47
sqljim <jim.m257@gmail.com>
2021-05-10 11:51:27 +0530

    Adding firebase back into package

--- scripts/database/Release/Release_1.19/DML/4_A041_PatientTableChanges.sql
+++ scripts/database/Release/Release_1.19/DML/4_A041_PatientTableChanges.sql
@@ -9,9 +9,10 @@ BEGIN
   ADD COLUMN `SameAsNumber` INT NULL DEFAULT NULL AFTER `PatientCallOutcomeId`;
   
   IF (SELECT COUNT(1) FROM AAU.Patient WHERE PatientCallOutcomeId IS NOT NULL) = 0 THEN
+  
   UPDATE AAU.Patient p
   INNER JOIN AAU.EmergencyCase ec ON ec.EmergencyCaseId = p.EmergencyCaseId
-  SET p.PatientCallOutcomeId = ec.CallOutcomeId;
+  SET p.PatientCallOutcomeId = ec.CallOutcomeId, p.SameAsNumber = ec.SameAsEmergencyCaseId;
   
   END IF;
   
@@ -19,3 +20,5 @@ END //
 DELIMITER ;
 CALL `?`();
 DROP PROCEDURE `?`;
+
+-- We need to also make sure the update worked successfully.
