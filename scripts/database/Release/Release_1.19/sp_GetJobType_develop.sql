0232fce67b911faddfd5b2fc9c15d6af8f0de926
Arpit <arpit.trivedi98@gmail.com>
2021-05-10 15:21:58 +0530

    added a new Stored proc in the release folder

---
+++ scripts/database/Release/Release_1.19/sp_GetJobType_develop.sql
@@ -0,0 +1,27 @@
+DELIMITER !!
+
+DROP PROCEDURE IF EXISTS AAU.sp_GetJobType !!
+
+DELIMITER $$
+CREATE PROCEDURE AAU.sp_GetJobType(IN prm_UserName VARCHAR(45))
+BEGIN
+
+/*
+Created By: Arpit Trivedi
+Created On: 27/10/2020
+Purpose: To get the UserjobType data for dropdown.
+*/
+
+DECLARE vOrganisationId INT;
+
+SELECT OrganisationId INTO vOrganisationId FROM AAU.User
+WHERE Username = prm_Username;
+
+
+SELECT DISTINCT jt.JobTypeId , jt.Title FROM aau.JobType jt
+WHERE jt.OrganisationId = vOrganisationId ;
+
+END$$
+DELIMITER ;
+
+
