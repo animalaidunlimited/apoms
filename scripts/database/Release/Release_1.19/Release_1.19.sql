a671cd4028b49f72c916e1556ad06d2c9fe6b0e6
Ankit Singh <ankitsingh.udaipur@gmail.com>
2021-05-12 13:20:38 +0530

    Added Insert and update patient procedure

--- scripts/database/Release/Release_1.19/Release_1.19.sql
+++ scripts/database/Release/Release_1.19/Release_1.19.sql
@@ -1667,8 +1667,6 @@ DELIMITER ;
 DELIMITER !!
 
 DROP PROCEDURE IF EXISTS AAU.sp_GetStreetTreatWithVisitDetailsByPatientId!!
-
-DELIMITER $$
 DELIMITER $$
 CREATE PROCEDURE AAU.sp_GetStreetTreatWithVisitDetailsByPatientId(IN prm_PatientId INT)
 BEGIN
@@ -2703,78 +2701,7 @@ CALL AAU.sp_GetOutstandingRescueByEmergencyCaseId(prm_EmergencyCaseId, NULL);
 
 END$$
 DELIMITER ;
-DELIMITER !!
 
-DROP PROCEDURE IF EXISTS AAU.sp_UpdatePatient !!
-
-DELIMITER $$
-CREATE PROCEDURE AAU.sp_UpdatePatient(
-									IN prm_UserName VARCHAR(64),
-									IN prm_PatientId INT,
-									IN prm_EmergencyCaseId INT,
-									IN prm_Position INT,
-									IN prm_AnimalTypeId INT,									
-                                    IN prm_IsDeleted INT,
-                                    IN prm_TagNumber VARCHAR(45),
-                                    IN prm_PatientCallOutcomeId INT,
-                                    IN prm_SameAsNumber INT
-)
-BEGIN
-
-DECLARE vOrganisationId INT;
-DECLARE vPatientExists INT;
-DECLARE vPatientId INT;
-DECLARE vTagNumber VARCHAR(45);
-DECLARE vExistingTagNumber VARCHAR(45);
-DECLARE vSuccess INT;
-SET vTagNumber = NULL;
-
-SELECT COUNT(1) INTO vPatientExists FROM AAU.Patient WHERE PatientId <> prm_PatientId
-AND EmergencyCaseId = prm_EmergencyCaseId
-AND Position = prm_Position AND IsDeleted = 0;
-
-SELECT TagNumber INTO vExistingTagNumber FROM AAU.Patient WHERE PatientId = prm_PatientId;
-
-SELECT OrganisationId INTO vOrganisationId FROM AAU.User WHERE UserName = prm_Username LIMIT 1;
-
-IF vPatientExists = 0 THEN
-
-	UPDATE AAU.Patient SET
-			Position		= prm_Position,
-			AnimalTypeId	= prm_AnimalTypeId,
-			TagNumber		= IF(prm_IsDeleted = 1, NULL, UPPER(prm_TagNumber)),
-            PatientCallOutcomeId = prm_PatientCallOutcomeId,
-            SameAsNumber = prm_SameAsNumber,
-            IsDeleted		= prm_IsDeleted,
-            DeletedDate		= CASE
-								WHEN prm_IsDeleted = FALSE THEN NULL
-                                WHEN prm_IsDeleted = TRUE AND DeletedDate IS NULL THEN NOW()
-							  END								
-	WHERE PatientId = prm_PatientId;
-    
-    -- Now update the Census in case there were records entered there early.
-    IF IFNULL(prm_TagNumber, '') <> '' AND vExistingTagNumber <> prm_TagNumber THEN     
-		UPDATE AAU.Census SET TagNumber = prm_TagNumber WHERE PatientId = prm_PatientId;
-    END IF;
-    
-	INSERT INTO AAU.Logging (OrganisationId, UserName, RecordId, ChangeTable, LoggedAction, DateTime)
-	VALUES (vOrganisationId, prm_Username, prm_PatientId,'Patient','Update', NOW());
-               
-    SELECT 1,prm_TagNumber,prm_PatientId INTO vSuccess,vTagNumber,vPatientId;  
-    
-ELSEIF vPatientExists >= 1 THEN
-
-	SELECT 2 INTO vSuccess;
-
-ELSE
-
-	SELECT 3 INTO vSuccess;
-END IF;
-
-SELECT vPatientId AS patientId, vTagNumber, vSuccess AS success;
-
-END$$
-DELIMITER ;
 DELIMITER !!
 
 DROP PROCEDURE IF EXISTS AAU.sp_UpdatePatientCallerInteraction !!
@@ -3344,7 +3271,7 @@ DELIMITER ;
 DELIMITER !!
 
 DROP PROCEDURE IF EXISTS AAU.sp_UpsertVisit!!
-DROP PROCEDURE IF EXISTS AAU.sp_InsertAndUpdateStreetTreatCase!!
+DROP PROCEDURE IF EXISTS AAU.sp_InsertAndUpdateVisit!!
 
 DELIMITER $$
 CREATE PROCEDURE AAU.sp_UpsertVisit(
@@ -3436,6 +3363,165 @@ ELSE
 END IF;
 
 SELECT vSuccess AS success, prm_VisitId AS visitId, DATE_FORMAT(prm_VisitDate, '%Y-%m-%d') AS visitDate;
+
+END$$
+DELIMITER ;
+
+
+DELIMITER !!
+DROP PROCEDURE IF EXISTS AAU.sp_InsertPatient!!
+
+DELIMITER $$
+CREATE PROCEDURE AAU.sp_InsertPatient(
+IN prm_Username VARCHAR(128),
+IN prm_EmergencyCaseId INT,
+IN prm_Position INT,
+IN prm_AnimalTypeId INT,
+IN prm_TagNumber VARCHAR(45),
+IN prm_PatientCallOutcomeId  INT,
+IN prm_SameAsEmergencyCaseId INT
+)
+BEGIN
+
+DECLARE vOrganisationId INT;
+DECLARE vPatientExists INT;
+DECLARE vPatientId INT;
+DECLARE vTagExists INT;
+DECLARE vSuccess INT;
+DECLARE vTagNumber VARCHAR(20);
+
+SET vPatientExists = 0;
+SET vTagExists = 0;
+SET vTagNumber = NULL;
+
+
+SELECT COUNT(1) INTO vPatientExists FROM AAU.Patient WHERE EmergencyCaseId = prm_EmergencyCaseId AND Position = prm_Position AND IsDeleted = 0;
+
+SELECT COUNT(1) INTO vTagExists FROM AAU.Patient WHERE TagNumber = prm_TagNumber;
+
+SELECT OrganisationId INTO vOrganisationId FROM AAU.User WHERE UserName = prm_Username LIMIT 1;
+
+IF vPatientExists = 0 AND vTagExists = 0 THEN
+
+	INSERT INTO AAU.Patient
+		(
+			OrganisationId,        
+			EmergencyCaseId,
+			Position,
+			AnimalTypeId,
+			TagNumber,
+            PatientCallOutcomeId,
+            SameAsEmergencyCaseId
+		)
+		VALUES
+		(
+			vOrganisationId,        
+			prm_EmergencyCaseId,
+			prm_Position,
+			prm_AnimalTypeId,
+			UPPER(prm_TagNumber),
+            prm_PatientCallOutcomeId,
+            prm_SameAsEmergencyCaseId
+		);
+          
+	SELECT 1 INTO vSuccess;
+    SELECT LAST_INSERT_ID(),prm_TagNumber INTO vPatientId,vTagNumber;
+    
+    IF IFNULL(prm_TagNumber,'') <> '' THEN    
+		UPDATE AAU.Census SET PatientId = vPatientId WHERE TagNumber = vTagNumber;    
+    END IF;
+    
+    
+
+	INSERT INTO AAU.Logging (OrganisationId, UserName, RecordId, ChangeTable, LoggedAction, DateTime)
+	VALUES (vOrganisationId, prm_Username,vPatientId,'Patient','Insert', NOW());
+
+ELSEIF vPatientExists > 0 THEN
+
+	SELECT 2 INTO vSuccess;
+
+ELSEIF vTagExists > 0 THEN
+    
+	SELECT 3 INTO vSuccess;
+    
+ELSE
+
+	SELECT 4 INTO vSuccess;
+END IF;
+
+SELECT vPatientId AS patientId, vSuccess AS success , vTagNumber;
+
+END$$
+DELIMITER ;
+
+
+DELIMITER !!
+DROP PROCEDURE IF EXISTS AAU.sp_UpdatePatient!!
+DELIMITER $$
+CREATE PROCEDURE AAU.sp_UpdatePatient(
+									IN prm_UserName VARCHAR(64),
+									IN prm_PatientId INT,
+									IN prm_EmergencyCaseId INT,
+									IN prm_Position INT,
+									IN prm_AnimalTypeId INT,									
+                                    IN prm_IsDeleted INT,
+                                    IN prm_TagNumber VARCHAR(45),
+                                    IN prm_PatientCallOutcomeId INT,
+                                    IN prm_SameAsEmergencyCaseId INT
+)
+BEGIN
+
+DECLARE vOrganisationId INT;
+DECLARE vPatientExists INT;
+DECLARE vPatientId INT;
+DECLARE vTagNumber VARCHAR(45);
+DECLARE vExistingTagNumber VARCHAR(45);
+DECLARE vSuccess INT;
+SET vTagNumber = NULL;
+
+SELECT COUNT(1) INTO vPatientExists FROM AAU.Patient WHERE PatientId <> prm_PatientId
+AND EmergencyCaseId = prm_EmergencyCaseId
+AND Position = prm_Position AND IsDeleted = 0;
+
+SELECT TagNumber INTO vExistingTagNumber FROM AAU.Patient WHERE PatientId = prm_PatientId;
+
+SELECT OrganisationId INTO vOrganisationId FROM AAU.User WHERE UserName = prm_Username LIMIT 1;
+
+IF vPatientExists = 0 THEN
+
+	UPDATE AAU.Patient SET
+			Position		= prm_Position,
+			AnimalTypeId	= prm_AnimalTypeId,
+			TagNumber		= IF(prm_IsDeleted = 1, NULL, UPPER(prm_TagNumber)),
+            PatientCallOutcomeId = prm_PatientCallOutcomeId,
+            SameAsEmergencyCaseId = prm_SameAsEmergencyCaseId,
+            IsDeleted		= prm_IsDeleted,
+            DeletedDate		= CASE
+								WHEN prm_IsDeleted = FALSE THEN NULL
+                                WHEN prm_IsDeleted = TRUE AND DeletedDate IS NULL THEN NOW()
+							  END								
+	WHERE PatientId = prm_PatientId;
+    
+    -- Now update the Census in case there were records entered there early.
+    IF IFNULL(prm_TagNumber, '') <> '' AND vExistingTagNumber <> prm_TagNumber THEN     
+		UPDATE AAU.Census SET TagNumber = prm_TagNumber WHERE PatientId = prm_PatientId;
+    END IF;
+    
+	INSERT INTO AAU.Logging (OrganisationId, UserName, RecordId, ChangeTable, LoggedAction, DateTime)
+	VALUES (vOrganisationId, prm_Username, prm_PatientId,'Patient','Update', NOW());
+               
+    SELECT 1,prm_TagNumber,prm_PatientId INTO vSuccess,vTagNumber,vPatientId;  
+    
+ELSEIF vPatientExists >= 1 THEN
+
+	SELECT 2 INTO vSuccess;
+
+ELSE
+
+	SELECT 3 INTO vSuccess;
+END IF;
+
+SELECT vPatientId AS patientId, vTagNumber, vSuccess AS success;
 
 END$$
 DELIMITER ;
