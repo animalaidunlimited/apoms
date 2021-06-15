1be01157ef6057f7415ab93685a591b76709d2e4
sqljim <jim.m257@gmail.com>
2021-06-15 11:34:21 +0530

    Adding sort order to Call Outcome

--- scripts/database/Release/Release_1.19/Release_1.19.sql
+++ scripts/database/Release/Release_1.19/Release_1.19.sql
@@ -3925,4 +3925,20 @@ SELECT	IFNULL(TotalActiveCases,0) AS TotalActiveCases,
         IFNULL(NoVisits,0) AS NoVisits;
 
 END$$
-DELIMITER ;
+
+DELIMITER !!
+
+DROP PROCEDURE IF EXISTS AAU.sp_GetOutcomes!!
+
+DELIMITER $$
+CREATE PROCEDURE AAU.sp_GetOutcomes(IN prm_username VARCHAR(45))
+BEGIN
+
+DECLARE vOrganisationId INT;
+SET vOrganisationId = 1;
+
+SELECT OrganisationId INTO vOrganisationId FROM AAU.User WHERE UserName = prm_Username LIMIT 1;
+
+SELECT CallOutcomeId, CallOutcome, SortOrder FROM AAU.CallOutcome WHERE OrganisationId = vOrganisationId AND IsDeleted = false;
+
+END$$
