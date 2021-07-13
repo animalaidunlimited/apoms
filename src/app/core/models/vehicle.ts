c9710715fad79668bb09f70694060603e606d814
sqljim <jim.m257@gmail.com>
2021-07-13 18:08:42 +0530

    WIP vehicle shift assigner

--- src/app/core/models/vehicle.ts
+++ src/app/core/models/vehicle.ts
@@ -4,10 +4,8 @@ import { User } from "./user";
 export interface VehicleShift {
     vehicleShiftId: number;
     vehicleId: number;
-    startTime: Date;
-    endTime: Date;
-    shiftStartTimeString?: string;
-    shiftEndTimeString?: string;
+    shiftStartTime: Date;
+    shiftEndTime: Date;
     left?: number;
     length?: number;
     vehicleStaff: User[];
