b765775ed61c1d0f7b8e4c11b5cbbcce55e52e25
Ankit Singh <ankitsingh.udaipur@gmail.com>
2021-07-14 10:18:48 +0530

    folder structure changes for emergency register ambulance component

--- src/app/modules/emergency-register/components/outstanding-case-board2/outstanding-case-board2.module.ts
+++ src/app/modules/emergency-register/components/outstanding-case-board2/outstanding-case-board2.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material-module';
 import { OutstandingCaseBoard2Component } from '../outstanding-case-board2/outstanding-case-board2.component';
-
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { SearchResultCardModule } from 'src/app/core/components/search-result-card/search-result-card.module';
 import { MatChipsModule } from '@angular/material/chips';
 import { SharedPipesModule } from 'src/app/shared-pipes.module';
-import { EmergencyRegisterAmbulanceComponent } from '../tab-bar/emergency-register-ambulance/emergency-register-ambulance.component';
+import { EmergencyRegisterAmbulanceComponent } from '../emergency-register-ambulance/emergency-register-ambulance.component';
 
 @NgModule({
     declarations: [
