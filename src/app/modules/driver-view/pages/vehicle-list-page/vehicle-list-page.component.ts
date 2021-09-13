15e0b16f231b32e388d06607fd7132f876803e30
Ankit Singh <ankitsingh.udaipur@gmail.com>
2021-09-13 18:11:59 +0530

    imgPreview

--- src/app/modules/driver-view/pages/vehicle-list-page/vehicle-list-page.component.ts
+++ src/app/modules/driver-view/pages/vehicle-list-page/vehicle-list-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
@@ -7,6 +7,7 @@ import { DropdownService } from 'src/app/core/services/dropdown/dropdown.service
 import { SnackbarService } from 'src/app/core/services/snackbar/snackbar.service';
 import { VehicleService } from '../../services/vehicle.service';
 import { AuthService } from 'src/app/auth/auth.service';
+import { MediaPasteService } from './../../../../core/services/navigation/media-paste/media-paste.service';
 
 
 
@@ -16,6 +17,9 @@ import { AuthService } from 'src/app/auth/auth.service';
   styleUrls: ['./vehicle-list-page.component.scss']
 })
 export class VehicleListPageComponent implements OnInit {
+
+
+  imgURL!:string | ArrayBuffer | null;
 
   vehicleStatus: VehicleStatus[] = [
     {VehicleStatusId: 1, VehicleStatus: 'Active'},
@@ -47,7 +51,9 @@ export class VehicleListPageComponent implements OnInit {
     smallAnimalCapacity: [],
     vehicleStatusId:[],
     minRescuerCapacity:[],
-    maxRescuerCapacity:[]
+    maxRescuerCapacity:[],
+    organisationId: this.authService.getOrganisationId(),
+    vehicleImage:''
   });
 
 
@@ -55,7 +61,8 @@ export class VehicleListPageComponent implements OnInit {
               private fb: FormBuilder,
               private vehicleService: VehicleService,
               private snackBar: SnackbarService,
-              private authService:AuthService) { }
+              private authService:AuthService,
+              private mediaPaste:MediaPasteService) { }
 
   ngOnInit(): void {
     this.vehicleType$ = this.dropdown.getVehicleType();
@@ -64,10 +71,10 @@ export class VehicleListPageComponent implements OnInit {
   }
 
   Submit(vehicleForm: FormGroup) {
-    const organisationId = this.authService.getOrganisationId();
-    this.vehicleService.upsertVehicleListItem({...vehicleForm.value, organisationId}).then(response=> {
+
+    this.vehicleService.upsertVehicleListItem(vehicleForm.value).then(response=> {
       if(response.success === -1) {
-        this.snackBar.errorSnackBar('Communication error see adim', 'Ok');
+        this.snackBar.errorSnackBar('Communication error see admin', 'Ok');
       }
       else {
         if(response.success === 1) {
@@ -114,5 +121,48 @@ export class VehicleListPageComponent implements OnInit {
       });
     }
 
+  }
+
+  uploadFile($event:any) : void {
+  
+    /* for(const file of $event.target.files)
+    {
+
+      let imgURL;
+
+      var reader = new FileReader();
+  
+        reader.readAsDataURL(this.uploader.nativeElement.files[0]); 
+        reader.onload = (_event) => { 
+          imgURL = reader.result; 
+        }
+
+        console.log(imgURL )
+      /* const mediaItem = this.mediaPaste.handleImageUpload(file);
+      
+      mediaItem?.url.subscribe(url => {
+        this.vehicleListForm.get('vehicleImage')?.setValue(url);
+        console.log(url);
+      })
+  
+    } */
+  
+    if ($event.target.files.length === 0)
+      return;
+
+    var mimeType = $event.target.files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      return;
+    }
+
+    var reader = new FileReader();
+ 
+    reader.readAsDataURL($event.target.files[0]); 
+    reader.onload = (_event) => { 
+      this.imgURL = reader.result; 
+    }
+
+    
+  
   }
 }
